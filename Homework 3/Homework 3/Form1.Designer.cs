75bab544f3ee4f93637e2958f508589145a26dd6
Infiniteez <andreamac2000@gmail.com>
2022-10-19 11:04:50 +0200

    Add files via upload

---
+++ Homework 3/Homework 3/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Homework_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(694, 409);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Parse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 409);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(112, 413);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(576, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 272);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 290);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(776, 113);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "CSV Parser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+    }
+}
---
+++ Homework 3/Homework 3/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Numerics;
+using static System.Windows.Forms.LinkLabel;
+
+namespace Homework_3
+{
+    public partial class Form1 : Form
+    {
+        Random random;
+        IEnumerable<string> lines;
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            //label1.Text = (random.Next() % 10).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Int64 range1=0, range2=0, range3=0;
+            int limit1=500, limit2=1000;
+            int column=3;
+            bool skipHeader = true;
+
+            richTextBox1.Clear();
+            if (lines == null) return;
+            foreach (var line in lines.Skip(skipHeader?1:0))
+            {
+                var list = line.Split(',');
+
+                if (double.Parse(list[column], CultureInfo.InvariantCulture) < limit1)
+                    range1++;
+                else if (double.Parse(list[column], CultureInfo.InvariantCulture) < limit2)
+                    range2++;
+                else
+                    range3++;
+
+                richTextBox1.AppendText(list[column] + Environment.NewLine);
+                richTextBox2.Clear();
+                richTextBox2.AppendText("IP ANALIZZATI: " + (range1+range2+range3) + Environment.NewLine);
+                richTextBox2.AppendText("Hanno inviato meno di " + limit1 + " pacchetti: " + range1 + " sorgenti IP;" + Environment.NewLine);
+                richTextBox2.AppendText("Hanno inviato tra " + limit1 + " e " + limit2 + " pacchetti: " + range2 + " sorgenti IP;" + Environment.NewLine);
+                richTextBox2.AppendText("Hanno inviato più di " + limit2 + " pacchetti: " + range3 + " sorgenti IP." + Environment.NewLine);
+
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+                string filename = openFileDialog1.SafeFileName;
+                try
+                {
+                    textBox1.Text = filename;
+                    lines = File.ReadLines(path);
+                    button1.Enabled = true;
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+    }
+}
